4fefdbaecc2538b0af4ca84d95f4af176f3c9340
Henry Bautista <henry.bautista.prado@icloud.com>
2023-03-25 20:08:55 -0600

    creando skills de habilidades

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
-let menuVisible = false;
-//Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
-        menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
-    }
-}
+//Funcion que aplica las animaciones de las habilidades
 
-function seleccionar(){
-    //oculta el menu una vez que seleccione una opción
-    document.getElementById("nav").classList = "";
-    menuVisible = false;
-}
-// Funcion que aplica animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByName("progreso")
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("react");
-        habilidades[3].classList.add("vite");
-        habilidades[4].classList.add("git");
-    } 
-}
-
-// detecto scrolling para aplicar la animación
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
 }
